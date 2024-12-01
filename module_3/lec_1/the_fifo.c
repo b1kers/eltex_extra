97a7d06e6028bc5315ac41cfe03ea54e92efac73
b1kers <pas2shkov.ilya@gmail.com>
2024-12-01 23:30:10 +0700

    fix an access rights

--- module_3/lec_1/the_fifo.c
+++ module_3/lec_1/the_fifo.c
@@ -15,19 +15,19 @@ int main(){
     unlink(fifo_fn);
     int fd_fifo; //=open(fifo_fn, mode);
     //int res = mkfifo(fifo_fn, mode);
-    int res = mknod(fifo_fn, 755, 0);
+    int res = mknod(fifo_fn, 0755, 0);
     printf("%d %d\n", fd_fifo, res);
     if (fd_fifo=open(fifo_fn, mode, O_NDELAY) == -1)
     {
 	fprintf(stderr, "Cannot open");
-	//exit(0);
+	exit(0);
 	//int a = 1;
    }
 	   fprintf(stdout, "OK\n");
     	   write(fd_fifo, text, strlen(text));
     if(read(fd_fifo, &buf, sizeof(buf)) == -1)
     {
-	fprintf(stderr, "Cannot open");
+	fprintf(stderr, "Cannot read");
     } else {
     	 printf("\n%s \n", buf);
     }
